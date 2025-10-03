d376c8f212b6b96977d2cb64365cca84661473fc
X3N0TEK <x3n0tek@proton.me>
2025-10-03 12:24:47 -0400

    added project files to begin pahse 1 0.0.3

--- falshcard/src/main/java/flashcards/Main.java
+++ falshcard/src/main/java/flashcards/Main.java
@@ -1,4 +1,8 @@
 package flashcards;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
